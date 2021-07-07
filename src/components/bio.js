ad436ec4a2a29fd37eac2528f9d498f4b21910db
= <=>
2021-07-07 10:43:35 +0200

    change bio layout

--- src/components/bio.js
+++ src/components/bio.js
@@ -1,23 +1,23 @@
 import * as React from "react";
 import { Container } from "react-bootstrap";
-import { container_bio } from "./layout.module.css";
+import { container_bio, bio_paragraph } from "./layout.module.css";
 
 const Bio = ({ idProp }) => {
     return (
         <Container fluid className={container_bio} id={idProp}>
 
 
-            <p>
+            {/* <p>
                 I currently work as a freelance <strong><h4>Front-End developer</h4></strong>.
-                </p><br />
-            <p>I enjoy designing and building beautiful applications, with a keen eye on <b>responsivness</b>.</p><br />
-            <p>The technologies I use the most are:</p>
+                </p><br /> */}
+            <h3>I enjoy designing and building beautiful applications, with a keen eye on <b>responsivness</b>.</h3><br />
+            <h4>The technologies I use the most are:</h4>
             <label>React.js</label><label>Node.js</label><label>Express</label><label>HTML</label><label>Javascript</label><label>CSS</label><br /><label>Bootstrap</label><label>Gatsby</label><label>Balsamiq</label><label>Figma</label><br />
 
-            <p>I was born in a small town in <b>Tuscany</b>, Italy (yes, that means <b>WINE</b> )and now I live in <b>Copenhagen</b> with my husband and daughter.
+            <div className={bio_paragraph}><p>I was born in a small town in <b>Tuscany</b>, Italy (yes, that means <b>WINE</b> )and now I live in <b>Copenhagen</b> with my husband and daughter.
                  While completing a PhD in Classics, I discovered a passion for <b>programming</b> and <b>design</b>.
 
-            </p>
+            </p></div>
 
 
 
