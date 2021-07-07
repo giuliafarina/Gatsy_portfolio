8671dd7f52db7a3c234d55a21cc78408c5a5f7c0
= <=>
2021-07-07 11:09:18 +0200

    add github icon projects section

--- src/components/bio.js
+++ src/components/bio.js
@@ -12,7 +12,7 @@ const Bio = ({ idProp }) => {
                 </p><br /> */}
             <h3>I enjoy designing and building beautiful applications, with a keen eye on <b>responsivness</b>.</h3><br />
             <h4>The technologies I use the most are:</h4>
-            <label>React.js</label><label>Node.js</label><label>Express</label><label>HTML</label><label>Javascript</label><label>CSS</label><br /><label>Bootstrap</label><label>Gatsby</label><label>Balsamiq</label><label>Figma</label><br />
+            <label>React.js</label><label>Node.js</label><label>Express</label><label>HTML</label><label>Javascript</label><label>CSS</label><br /><label>Bootstrap</label><label>Gatsby.js</label><label>Balsamiq</label><label>Figma</label><br />
 
             <div className={bio_paragraph}><p>I was born in a small town in <b>Tuscany</b>, Italy (yes, that means <b>WINE</b> )and now I live in <b>Copenhagen</b> with my husband and daughter.
                  While completing a PhD in Classics, I discovered a passion for <b>programming</b> and <b>design</b>.
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,13 +1,14 @@
 import * as React from "react";
 import { Container } from "react-bootstrap";
 import { container_projects } from "./layout.module.css"
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 const Projects = ({ idProp }) => {
     return (
         <Container fluid className={container_projects} id={idProp}>
             <p>Have a look at my projects:
-            </p>
-            <a href="https://github.com/giuliafarina">https://github.com/giuliafarina</a>
+            </p><br />
+            <a href="https://github.com/giuliafarina"><GitHubIcon /> https://github.com/giuliafarina</a>
 
 
         </Container>
