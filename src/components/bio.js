30161a4d79da52b871bf72cad3ed18f60153a56a
= <=>
2021-07-09 11:23:47 +0200

    import hamburger component

--- src/components/bio.js
+++ src/components/bio.js
@@ -7,11 +7,9 @@ const Bio = ({ idProp }) => {
         <Container fluid className={container_bio} id={idProp}>
 
 
-            {/* <p>
-                I currently work as a freelance <strong><h4>Front-End developer</h4></strong>.
-                </p><br /> */}
+
             <h3>I enjoy designing and building beautiful applications, <br />with a keen eye on <b>responsiveness</b>.</h3><br />
-            <p><h4>The technologies I use the most are:</h4></p>
+            <h4>The technologies I use the most are:</h4>
             <label>React.js</label><label>Node.js</label><label>Express</label><label>HTML</label><label>Javascript</label><label>CSS</label><br /><label>Bootstrap</label><label>Gatsby.js</label><label>Balsamiq</label><label>Figma</label><br />
             {/* <h4>About me</h4>
             <p className={bio_paragraph}>I was born in a small town in <b>Tuscany</b>, Italy (yes, that means <b>WINE</b> )and now I live in <b>Copenhagen</b> with my husband and daughter.
--- src/components/menu.js
+++ src/components/menu.js
@@ -1,28 +1,30 @@
 import * as React from "react";
-import { Container, Navbar, Nav } from "react-bootstrap";
-import { Link } from 'gatsby'
+import { useState } from "react";
+import { Navbar, Nav } from "react-bootstrap";
+
+
 import {
-    container,
-    navBar, navLinks,
+
+    navBar,
     navLinkItem,
     navLinkText
 } from "./layout.module.css";
 import { AnchorLink } from 'gatsby-plugin-anchor-links';
+import Hamburger from 'hamburger-react';
 
 
 
 
 
-const Menu = ({ children }) => {
+const Menu = ({ props }) => {
+    const [isOpen, setOpen] = useState(false)
+
     return (
         <Nav className="ml-auto justify-content-center">
             <Navbar bg="transparent" variant="dark" collapseOnSelect expand="lg" className={navBar + " justify-content-center"}>
+                <Hamburger className={Styles.burger} toggled={isOpen} toggle={setOpen} />
 
-                <Navbar.Toggle aria-controls="responsive-navbar-nav navbarScroll" />
-                <Navbar.Collapse id="responsive-navbar navbarScroll" className="order-md-1">
-
-
-
+                <Navbar.Collapse id="responsive-navbar navbarScroll" className={isOpen ? "show " : "" + "order-md-1"}>
 
                     <Nav.Link className={navLinkItem}>
                         <AnchorLink to="/#bio"
@@ -32,8 +34,6 @@ const Menu = ({ children }) => {
                         <AnchorLink to="/#projects" className={navLinkText}>Projects</AnchorLink>
                     </Nav.Link>
                     <Nav.Link className={navLinkItem}><AnchorLink to="/#contact" className={navLinkText}>Contact</AnchorLink></Nav.Link>
-
-
 
                 </Navbar.Collapse>
             </Navbar>
