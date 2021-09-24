8b53797789153058bf78c80f6044ff1a1e93f2c8
= <=>
2021-09-24 17:03:52 +0200

    toggle function create

--- src/components/hamburger.js
+++ src/components/hamburger.js
@@ -1,15 +1,13 @@
 import * as React from "react";
+import { useState } from "react";
 import * as styles from "../styles/hamburger.module.css";
 
 const Hamburger = () => {
+
+
     return <>
 
-        <div className={styles.burger}>
-            <span className={styles.line1} />
-            <span className={styles.line2} />
-            <span className={styles.line3} />
 
-        </div>
 
 
     </>
--- src/components/menu.js
+++ src/components/menu.js
@@ -3,28 +3,48 @@ import { useState } from "react";
 
 
 
+
 import * as styles from "../styles/layout.module.css";
 import { AnchorLink } from 'gatsby-plugin-anchor-links';
-import Hamburger from "./hamburger";
+import classNames from "classnames";
 
 
 
 
 
 const Menu = () => {
-    // const [isOpen, setOpen] = useState(false)
+    const [mobileNavOpen, setMobileNavOpen] = useState(false)
+
+
+    function handleClick() {
+        setMobileNavOpen(prev => !prev);
+
+
+    }
+
+
+
 
     return <><div className={styles.wrapper}>
 
         <nav className={styles.navBar}>
-            <ul className={styles.nav_links}>
+            <div className={styles.burger}>
+                <button className={styles.button_toggle_menu} onClick={handleClick}>{mobileNavOpen ? "Close" : "Open"}
+                    <span className={styles.line1} />
+                    <span className={styles.line2} />
+                    <span className={styles.line3} />
+                </button>
+
+
+            </div>
+            <ul className={`menuNav ${mobileNavOpen ? "showMenu" : ""}`} >
                 <li className={styles.navLinkItem}><AnchorLink className={styles.navLinkText} to="/#bio">About</AnchorLink></li>
                 <li className={styles.navLinkItem}><AnchorLink className={styles.navLinkText} to="/#projects">Projects</AnchorLink></li>
                 <li className={styles.navLinkItem}><AnchorLink className={styles.navLinkText} to="/#contact">Contacts</AnchorLink></li>
             </ul>
 
 
-        </nav></div><Hamburger /></>
+        </nav></div></>
 }
 {/* // <Nav className="ml-auto justify-content-center">
     //     <Navbar bg="transparent" variant="dark" collapseOnSelect expand="lg" className={navBar + " justify-content-center"}>
