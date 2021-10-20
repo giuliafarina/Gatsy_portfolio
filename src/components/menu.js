701644c806c833358aa26cc4eba711041f75a221
= <=>
2021-10-20 10:03:14 +0200

    update README

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,12 +1,9 @@
 import * as React from "react";
 import { useState } from "react";
-
-
-
-
 import * as styles from "../styles/layout.module.css";
 import { AnchorLink } from 'gatsby-plugin-anchor-links';
-import classNames from "classnames";
+import logo from "../images/Untitled.svg"
+
 
 
 
@@ -15,16 +12,11 @@ import classNames from "classnames";
 const Menu = () => {
     const [mobileNavOpen, setMobileNavOpen] = useState(false)
 
-
     function handleClick() {
         setMobileNavOpen(prev => !prev);
 
 
     }
-
-
-
-
     return <><div className={mobileNavOpen ? styles.wrapper_active : styles.wrapper}>
 
         <nav className={styles.navBar}>
@@ -38,6 +30,7 @@ const Menu = () => {
 
             </div>
             <ul className={mobileNavOpen ? styles.nav_links_active : styles.nav_links}>
+
                 <li className={styles.navLinkItem}><AnchorLink className={styles.navLinkText} to="/#bio">About</AnchorLink></li>
                 <li className={styles.navLinkItem}><AnchorLink className={styles.navLinkText} to="/#projects">Projects</AnchorLink></li>
                 <li className={styles.navLinkItem}><AnchorLink className={styles.navLinkText} to="/#contact">Contacts</AnchorLink></li>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,19 @@
 
-
 import * as React from 'react'
-
 import Layout from '../components/layout';
-
-// Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-
-
-
-
-
-
-// Step 2: Define your component
 const IndexPage = () => {
   return (
     <>
       <Layout>
-
-
-
       </Layout>
-
-
-
     </>
   )
 }
 
-// Step 3: Export your component
+
 export default IndexPage
 
 
