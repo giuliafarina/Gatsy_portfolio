9f4cd6634b246b1e0963558219f1b6604ad3db5a
= <=>
2021-11-05 11:15:59 +0100

    add favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import * as React from 'react';
 import { Helmet } from "react-helmet"
 import Layout from '../components/layout';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import favicon from "../images/favicon.svg";
 
 
 
@@ -16,7 +17,8 @@ const IndexPage = () => {
         <meta name="description" content="Giulia Farina- Frontend Developer" />
         <meta property="og:title" content="Giulia Farina- Frontend Developer" />
         <meta property="og:description" content="Hi! I'm Giulia, a freelance frontend developer based in Copenhagen." />
-        <link rel="shortcut icon" type="image/svg+xml" href="../images/logoGF.svg" />
+        <meta property="og:site_name" content="www.giuliafarina.com" />
+        <link rel="icon" type="image/svg+xml" href={favicon} />
         <title>Giulia Farina | Frontend Developer</title>
       </Helmet>
       <Layout>
