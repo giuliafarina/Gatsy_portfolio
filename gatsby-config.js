ee06447ff7fec429f836ccbc1d7f61a5e5543aba
= <=>
2021-12-02 11:05:37 +0100

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,17 @@ module.exports = {
   siteMetadata: {
     title: "My Portfolio",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud", `gatsby-plugin-react-helmet`],
+  plugins: ["gatsby-plugin-gatsby-cloud", `gatsby-plugin-react-helmet`,
+
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-213980477-2",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+      },
+    },
+  ],
 
 };
